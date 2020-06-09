f4997ca9e1c3e21ba96518370e4c6305e6864211
Samuel.pschr <samuel.pschr@eduge.ch>
2020-06-09 14:30:42 +0200

    ajout commentaire credentials.php

--- db/credentials.php
+++ db/credentials.php
@@ -6,8 +6,13 @@
 // Site de critique de livres
 // ==========================================
 
+// Adresse de l'hôte
 define("DB_HOST", "localhost");
+// Nom de la base de données
 define("DB_NAME", "dbtpi");
+// Numéro de port
 define("DB_PORT", "3306");
+// Nom de l'utilisateur / administrateur de la base
 define("DB_USER", "adminTpi");
+// mot de passe de l'utilisateur
 define("DB_PASS", "tpi2020");
